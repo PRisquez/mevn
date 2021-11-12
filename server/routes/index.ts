5fdea3ba5d340dc0a6f7536355ff7a26ddfd64a8
PRisquez <prisquez@emergya.com>
2021-11-12 09:43:47 +0100

    Initial commit

---
+++ server/routes/index.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/tasks', (req, res) => {
+    res.send("Hello world!");
+});
+
+
+router.post('/tasks', (req, res) => {
+    res.send("Hello world!");
+});
+
+router.get('/tasks/:id', (req, res) => {
+    res.send("Hello world!");
+});
+
+router.delete('/tasks/:id', (req, res) => {
+    res.send("Hello world!");
+});
+
+router.put('/tasks/:id', (req, res) => {
+    res.send("Hello world!");
+});
+export default router;

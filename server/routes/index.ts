02032462d4a849532705d9c847054b9522d15bd1
PRisquez <prisquez@emergya.com>
2021-11-12 15:24:54 +0100

    server finished

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,25 +1,54 @@
 import { Router } from "express";
+import Task from "../models/Task";
 
 const router = Router();
 
-router.get('/tasks', (req, res) => {
-    res.send("Hello world!");
+router.get('/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.send(tasks);
 });
 
 
-router.post('/tasks', (req, res) => {
-    res.send("Hello world!");
+router.post('/tasks', async (req, res) => {
+    const { title, description } = req.body;
+    const task = new Task({title, description});
+    await task.save();
+    res.json(task);
 });
 
-router.get('/tasks/:id', (req, res) => {
-    res.send("Hello world!");
+router.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) return res.status(404).json({"message": "Task not found"});
+        res.send(task);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
 });
 
-router.delete('/tasks/:id', (req, res) => {
-    res.send("Hello world!");
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) return res.status(404).json({"message": "Task not found"});
+        return res.send(task);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
 });
 
-router.put('/tasks/:id', (req, res) => {
-    res.send("Hello world!");
+router.put('/tasks/:id', async (req, res) => {
+    
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!task) return res.status(404).json({"message": "Task not found"});
+        return res.send(task);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
 });
 export default router;
